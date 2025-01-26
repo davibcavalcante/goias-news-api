33c89143f550b6c5d3829dbd028ace73d6ab8846
Davi Cavalcante <davib506@gmail.com>
2025-01-26 13:53:54 -0300

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const cors = require("cors");
+const xml2js = require('xml2js');
+const axios = require('axios');
+
+const app = express();
+app.use(cors());
+
+app.get('/', (req, res)  => {
+  res.send('Hello, World!');
+});
+
+app.get("/api/rss", async (req, res) => {
+  const fetchRSSFeed = async () => {
+    try {
+      const response = await axios.get('https://agenciabrasil.ebc.com.br/feed/ultimasnoticias/feed.xml');
+      const xmlText = response.data;
+
+      const parser = new xml2js.Parser();
+      parser.parseString(xmlText, (err, result) => {
+        if (err) {
+          console.error('Erro ao parsear XML:', err);
+          res.status(500).json({ error: 'Erro ao processar o feed' });
+          return;
+        }
+
+        const items = result.rss.channel[0].item;
+        const formattedItems = items.map((item) => ({
+          title: item.title[0],
+          link: item.link[0],
+          description: item.description ? item.description[0] : 'Sem descrição',
+          image: item['imagem-destaque'] ? item['imagem-destaque'][0] : null,
+          pubDate: item.pubDate ? item.pubDate[0] : 'Data não disponível',
+        }));
+
+        res.json(formattedItems);
+      });
+    } catch (error) {
+      console.error('Erro ao buscar feed:', error);
+      res.status(500).json({ error: 'Erro ao buscar o feed' });
+    }
+  };
+
+  fetchRSSFeed();
+});
+
+app.listen(3000, () => {
+  console.log("Servidor rodando na porta 3000");
+});
